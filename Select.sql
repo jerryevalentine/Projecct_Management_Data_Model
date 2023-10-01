0d1aa476c13eb25a6a98e67ea88977fca5d54b52
jerryevalentine <134810823+jerryevalentine@users.noreply.github.com>
2023-10-01 09:59:45 -0700

    Add files via upload

---
+++ Select.sql
@@ -0,0 +1,23 @@
+SELECT *
+FROM dbo.Programs;
+
+SELECT *
+FROM dbo.Projects;
+
+SELECT *
+FROM dbo.FunctionalRequirements;
+
+SELECT *
+FROM dbo.DesignSpecifications;
+
+SELECT *
+FROM dbo.DevelopmentItems;
+
+SELECT *
+FROM dbo.TestItems;
+
+SELECT *
+FROM dbo.DeploymentItems;
+
+SELECT *
+FROM dbo.BusinessAsUsualItems;
